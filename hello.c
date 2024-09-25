bba4a59dbc2bc8774eba2b55a4d0277f9b3e8666
Dharav <dharav9shah@gmail.com>
2024-09-25 01:54:28 -0400

    THir

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 int main(){
         printf("Hello World");
-        ptintf("trial");
+        printf("trial");
 }
 
 ~                                                                               
