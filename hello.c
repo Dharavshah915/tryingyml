771e211ed4626c7cf031bdb51db2e33e45ccb281
Dharav <dharav9shah@gmail.com>
2024-09-25 01:47:39 -0400

    Second

---
+++ hello.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+        printf("Hello World");
+        ptintf("trial");
+}
+
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+-- INSERT --                                                  6,2           All
+name: build script for quiz.c
+
+on:
+  push:
+    branches: [ "main" ]
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v4
+
+      - name: build quiz
+        run: gcc -o quiz quiz.c
+
+      - name: upload binary
+        uses: actions/upload-artifact@v4
+        with:
+          name: quiz
+          path: ./quiz
+
+
+~                                                                               
+-- INSERT --                                                  22,1          All
+
